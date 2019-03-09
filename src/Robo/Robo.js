91eff24ceb59eac33edf2a0929a4a1bc0b70f3a8
Robert Hudec <hudec@offeris.com>
2019-03-09 22:05:45 +0100

    images

--- src/Robo/Robo.js
+++ src/Robo/Robo.js
@@ -1,5 +1,16 @@
+import Player from "./Player";
+import * as PIXI from "pixi.js";
+
 export default class Robo{
-    constructor(app){
+    constructor(/*PIXI*/app){
         this.app = app;
+        this.loader = new PIXI.Loader();
+        this.player = new Player(this.loader, app);
+
+        app.ticker.add(delta => this.gameLoop(delta));
+    }
+
+    gameLoop = (delta) => {
+        this.player.moveSprite();
     }
 }
